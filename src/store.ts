6e06cdecbbcade3b86eeb205725a9eafd4d59a53
Gnarkill33 <gnarkill33@gmail.com>
2026-03-17 23:58:40 +0300

    feat: add store and some methods

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,33 @@
-import { type Product } from "./types.ts";
+import { type ProductType } from "./types.ts";
+import { randomUUID } from "node:crypto";
 
 export class Store {
-  private products: Product[] = [];
+  private products: ProductType[] = [
+    {
+      id: "1",
+      name: "string",
+      description: "string",
+      price: 24,
+      category: "string",
+      inStock: true,
+    },
+  ];
 
   getAllProducts() {
     return [...this.products];
   }
+
+  getProductById(id: string) {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) return null;
+    return { ...product };
+  }
+
+  addProduct(newProduct: Omit<ProductType, "id">) {
+    const newProductWithId = { ...newProduct, id: randomUUID() };
+    this.products.push(newProductWithId);
+    return newProductWithId;
+  }
 }
+
+export const store = new Store();
