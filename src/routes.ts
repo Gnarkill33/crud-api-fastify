73771e506b88d9b9a20b086bc5d1c3ab981ae279
Gnarkill33 <gnarkill33@gmail.com>
2026-03-17 23:59:57 +0300

    feat: add some routes to plugin

---
+++ src/routes.ts
@@ -0,0 +1,58 @@
+import { controller } from "./controller.ts";
+import type { FastifyInstance, FastifyPluginCallback } from "fastify";
+
+const Product = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+    description: { type: "string" },
+    price: { type: "number" },
+    category: { type: "string" },
+    inStock: { type: "boolean" },
+  },
+};
+
+const getAllProductsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        products: Product,
+      },
+    },
+  },
+  handler: controller.getAllProducts,
+};
+
+const getProductByIdOpts = {
+  schema: {
+    response: {
+      200: Product,
+    },
+  },
+  handler: controller.getProductById,
+};
+
+const postProductOpts = {
+  schema: {
+    response: {
+      201: Product,
+    },
+  },
+  handler: controller.addProduct,
+};
+
+export const useRoutes: FastifyPluginCallback = (
+  fastify: FastifyInstance,
+  _,
+  done,
+) => {
+  fastify.get("/api/products", getAllProductsOpts);
+
+  fastify.get("/api/products/:productId", getProductByIdOpts);
+
+  fastify.post("/api/products", postProductOpts);
+
+  done();
+};
