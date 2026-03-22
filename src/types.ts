d8d6c199547181c3e682b83aab91eac6a10d3c96
Gnarkill33 <gnarkill33@gmail.com>
2026-03-22 23:41:29 +0300

    feat: add types for workers and requests

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import type { FastifyRequest } from "fastify";
+import { Worker } from "node:cluster";
 
 export interface ProductType {
   id: string;
@@ -15,3 +16,22 @@ export type ProductRequest = FastifyRequest<{
   Body: ProductTypeNoId;
   Params: { productId: string };
 }>;
+
+export interface PendingRequest {
+  resolve: (value: ProductType | ProductType[] | boolean | null) => void;
+  reject: (reason?: Error) => void;
+  timeout: NodeJS.Timeout;
+}
+export interface WorkerMessage {
+  type: string;
+  messageId: number;
+  id?: string;
+  product?: ProductTypeNoId;
+  updates?: Partial<ProductTypeNoId>;
+  result?: ProductType | ProductType[] | boolean | null;
+}
+
+export interface WorkerInfo {
+  worker: Worker;
+  port: number;
+}
